fa0f99393403cbdef095980e544354544af832bd
IamThiago-IT <thiagodossantos315@gmail.com>
2021-02-13 15:55:04 -0300

    ✨  Exercise 1 from Module 1

---
+++ javascript/Módulo 1/exercício-1.js
@@ -0,0 +1,11 @@
+var endereço = {
+  rua: "Rua dos pinheiros",
+  numero: 1293,
+  bairro: "Centro",
+  cidade: "São paulo",
+  uf: "SP",
+};
+
+console.log(
+  `O usuário mora em ${endereço.cidade} / ${endereço.uf}, no bairro ${endereço.bairro} na rua "${endereço.rua}" com o n° ${endereço.numero}"`
+);
